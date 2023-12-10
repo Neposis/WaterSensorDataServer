6c221869fd4e3dd56dfc836f690bad4cdccf7584
Neposis <davidscreation1@gmail.com>
2023-12-10 20:00:57 +0000

    Started making the server somewhat useful

---
+++ src/client.js
@@ -0,0 +1,38 @@
+const WebSocket = require('ws');
+
+const websocketServerUrl = 'ws://localhost:433';
+
+const ws = new WebSocket(websocketServerUrl);
+
+let connection_finished = false;
+
+
+ws.on('open', () => {
+    console.log('Connected to WebSocket server');
+});
+
+ws.on('message', (data) => {
+    if (!connection_finished) {
+        data = data.toString();
+        console.info(`Server: ${data}`);
+        if (data === "State your business!") {
+            ws.send("I'm a WebClient");
+            console.info("Me: I'm a WebClient");
+        } else if (data === "Ok fine, you can join in...") {
+            connection_finished = true;
+        } else if (data === "Go away, I don't know who you are!") {
+            console.error("Something went wrong, I got denied...");
+            ws.close();
+        }
+    } else {
+        console.log(`Received: ${data.toString()}`)
+    }
+});
+
+ws.on('close', () => {
+    console.log('Connection closed');
+});
+
+ws.on('error', (error) => {
+    console.error(`WebSocket error: ${error.message}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,12 +1,72 @@
 const { WebSocketServer } = require('ws')
-const wss = new WebSocketServer({ port: 443 });
 
-wss.on('connection', function connection(ws) {
-    console.log("Client connected")
+const wss = new WebSocketServer({ port: 433 });
 
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
+
+let index = 0;
+let connected_clients = {};
+let arduino_client;
+
+wss.on('connection', (ws) => {
+
+    let local_index = index;
+    let connection_msg = true;
+    let client_type;
+    index++;
+    ws.send("State your business!")
+
+    // ------------------------------------------ OnMessage -------------------------------------
+    ws.on('message', (data) => {
+
+
+        if (connection_msg) {
+            data = data.toString()
+
+            // Web Client
+            if (data === "I'm a WebClient") {
+                console.log("Connected: A WebClient")
+                client_type = "WebClient"
+                connected_clients[local_index] = ws;
+                ws.send("Ok fine, you can join in...")
+
+            // Arduino Client
+            } else if (data === "I'm an Arduino hehe") {
+                console.log("Connected: An Arduino... we've been expecting you!");
+                client_type = "Arduino"
+                arduino_client = ws;
+
+            // Unknown
+            } else {
+                console.log(`Could not figure out who this is, message: "${data}"`)
+                ws.send("Go away, I don't know what you are!");
+                client_type = "Unknown"
+                ws.close();
+                return;
+            }
+
+            connection_msg--;
+            return;
+        }
+
+
+        console.log(data.toString())
+
+
+    });
+
+    // ------------------------------------------ OnClose -------------------------------------
+    ws.on('close', () => {
+        if (client_type === "Unknown") {
+            console.log(`Forcefully closing connection`);
+        }
+        console.log(`${client_type} disconnected`);
+        delete connected_clients[local_index];
     });
 
 
+    ws.on('error', (error) => {
+        console.error(`${client_type} #${local_index} WebSocket error: ${error.message}`);
+    });
 });
+
+
